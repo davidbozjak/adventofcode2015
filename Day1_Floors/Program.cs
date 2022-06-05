5ec08004dd2de357a0d23baea8da43f37bf3c5e1
David Božjak <bozjak@gmail.com>
2022-06-05 22:23:27 +0200

    Added day 1

---
+++ Day1_Floors/Program.cs
@@ -0,0 +1,34 @@
+﻿var input = new InputProvider<string?>("Input.txt", GetString).First();
+
+int current = 0;
+int position = 1;
+
+foreach (char c in input)
+{
+    current += c switch
+    {
+        '(' => 1,
+        ')' => -1,
+        _ => throw new Exception()
+    };
+
+    if (current == -1)
+    {
+        Console.WriteLine($"Part 2: {position}");
+    }
+
+    position++;
+}
+
+Console.WriteLine($"Part 1: {current}");
+
+static bool GetString(string? input, out string? value)
+{
+    value = null;
+
+    if (input == null) return false;
+
+    value = input ?? string.Empty;
+
+    return true;
+}
