a529d616616497ec323810d637dfb166059921a8
David Božjak <bozjak@gmail.com>
2022-06-11 16:28:03 +0200

    Added Day 3

---
+++ Day3_DeliveryGrid/DelieryBoy.cs
@@ -0,0 +1,43 @@
+﻿using System.Drawing;
+
+class DelieryBoy : IWorldObject
+{
+    private readonly UniqueFactory<(int, int), (int, int)> visitedCoordinates;
+
+    public DelieryBoy()
+    {
+        this.visitedCoordinates = new(w => w);
+
+        this.X = 0;
+        this.Y = 0;
+        this.visitedCoordinates.GetOrCreateInstance((X, Y));
+    }
+
+    public Point Position => new(X, Y);
+
+    public char CharRepresentation => 'x';
+
+    public int X { get; private set; }
+
+    public int Y { get; private set; }
+
+    public int Z => 1;
+
+    public int NumberOfVisitedCoordinates => this.visitedCoordinates.AllCreatedInstances.Count;
+
+    public IEnumerable<(int, int)> VisitedCoordinates => this.visitedCoordinates.AllCreatedInstances;
+
+    public void MakeDelivery(char direction)
+    {
+        (X, Y) = direction switch
+        {
+            '<' => (X - 1, Y),
+            '>' => (X + 1, Y),
+            '^' => (X, Y - 1),
+            'v' => (X, Y + 1),
+            _ => throw new Exception()
+        };
+
+        var house = this.visitedCoordinates.GetOrCreateInstance((X, Y));
+    }
+}
---
+++ Day3_DeliveryGrid/Program.cs
@@ -0,0 +1,32 @@
+﻿var directions = new InputProvider<char[]?>("Input.txt", GetChars).Where(w => w != null).Cast<char[]>().SelectMany(w => w).ToArray();
+
+var santa = new DelieryBoy();
+
+foreach (var direction in directions)
+{
+    santa.MakeDelivery(direction);
+}
+
+Console.WriteLine($"Part 1: {santa.NumberOfVisitedCoordinates}");
+
+var deliverers = new[] { new DelieryBoy(), new DelieryBoy() };
+
+for (int directionIndex = 0, delivererIndex = 0; directionIndex < directions.Length; directionIndex++, delivererIndex = (delivererIndex + 1) % deliverers.Length)
+{
+    deliverers[delivererIndex].MakeDelivery(directions[directionIndex]);
+}
+
+var uniqueVisitedCoordinates = deliverers.SelectMany(w => w.VisitedCoordinates).ToHashSet();
+
+Console.WriteLine($"Part 2: {uniqueVisitedCoordinates.Count}");
+
+static bool GetChars(string? input, out char[]? value)
+{
+    value = null;
+
+    if (input == null) return false;
+
+    value = input.ToCharArray();
+
+    return true;
+}
