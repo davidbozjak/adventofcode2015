569b74256798baa3058c21e1994f9776b3b081ae
David Božjak <bozjak@gmail.com>
2022-06-11 16:44:18 +0200

    Added Day 4

---
+++ Day4_MD5/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Text;
+
+string secretKey = "yzbqklnj";
+
+var stringsToFind = new List<string>() { new string(Enumerable.Repeat('0', 5).ToArray()), new string(Enumerable.Repeat('0', 6).ToArray()) };
+
+using (var md5 = System.Security.Cryptography.MD5.Create())
+{
+    for (long i = 1; stringsToFind.Count > 0 ; i++)
+    {
+        var input = Encoding.ASCII.GetBytes(secretKey + i.ToString());
+        var hashbytes = md5.ComputeHash(input);
+
+        var hexRepresentation = Convert.ToHexString(hashbytes);
+
+        foreach (var stringToFind in stringsToFind)
+        {
+            var str = hexRepresentation[..stringToFind.Length];
+
+            if (str == stringToFind)
+            {
+                Console.WriteLine($"Found {str} on {i}");
+                stringsToFind.Remove(str);
+                break;
+            }
+        }
+    }
+}
