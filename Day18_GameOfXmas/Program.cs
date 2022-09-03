9e827e0ce031cb161d6ee59275625bdd7f8d6954
David Božjak <bozjak@gmail.com>
2022-09-03 11:52:47 +0200

    Refactored

--- Day18_GameOfXmas/Program.cs
+++ Day18_GameOfXmas/Program.cs
@@ -5,20 +5,19 @@ Regex numRegex = new(@"\d+");
 Regex hexColorRegex = new(@"#[0-9a-z][0-9a-z][0-9a-z][0-9a-z][0-9a-z][0-9a-z]");
 
 var wholeStringInput = new InputProvider<string?>("Input.txt", GetString).Where(w => w != null).Cast<string>().ToList();
-var cells = new List<Cell>();
 
-for (int y = 0; y < wholeStringInput.Count; y++)
-{
-    for (int x = 0; x < wholeStringInput[y].Length; x++)
-    {
-        var cell = new Cell(x, y);
-        cell.OverrideState(wholeStringInput[y][x] == '#');
-        cells.Add(cell);
-    }
-}
+Console.WriteLine("Part 1");
+
+var cells = InitializeCellsFromInput(wholeStringInput);
+
+RunSimulation(cells, Enumerable.Empty<Cell>(), 100);
+
+Console.WriteLine($"Part 1: {cells.Count(w => w.IsOn)}");
+
+cells = InitializeCellsFromInput(wholeStringInput);
 
 //override corners to be ON
-var lockedCells = new List<Cell>() 
+var lockedCells = new List<Cell>()
 {
     cells.Where(w => w.X == 0 && w.Y == 0).First(),
     cells.Where(w => w.X == wholeStringInput[0].Length - 1 && w.Y == 0).First(),
@@ -28,20 +27,8 @@ var lockedCells = new List<Cell>()
 
 lockedCells.ForEach(w => w.OverrideState(true));
 
-foreach (var cell in cells)
-{
-    cell.SetNeighbours(cells.Where(w => w != cell && Math.Abs(cell.X - w.X) <= 1 && Math.Abs(cell.Y - w.Y) <= 1));
-}
-
-var processableCells = cells.Except(lockedCells).ToList();
-
-for (int i = 0; i < 100; i++)
-{
-    processableCells.ForEach(w => w.ReevaluateState());
-    processableCells.ForEach(w => w.UpdateState());
-
-    Console.WriteLine($"Generation {i + 1}: {cells.Count(w => w.IsOn)} lights are on.");
-}
+RunSimulation(cells, lockedCells, 100);
+Console.WriteLine($"Part 2: {cells.Count(w => w.IsOn)}");
 
 
 static bool GetString(string? input, out string? value)
@@ -53,6 +40,42 @@ static bool GetString(string? input, out string? value)
     value = input ?? string.Empty;
 
     return true;
+}
+
+static List<Cell> InitializeCellsFromInput(List<string> wholeStringInput)
+{
+    var cells = new List<Cell>();
+
+    for (int y = 0; y < wholeStringInput.Count; y++)
+    {
+        for (int x = 0; x < wholeStringInput[y].Length; x++)
+        {
+            var cell = new Cell(x, y);
+            cell.OverrideState(wholeStringInput[y][x] == '#');
+            cells.Add(cell);
+        }
+    }
+
+    foreach (var cell in cells)
+    {
+        cell.SetNeighbours(cells.Where(w => w != cell && Math.Abs(cell.X - w.X) <= 1 && Math.Abs(cell.Y - w.Y) <= 1));
+    }
+
+    return cells;
+}
+
+static void RunSimulation(List<Cell> cells, IEnumerable<Cell> lockedCells, int noOfIterations, bool printAtEveryStep = false)
+{
+    var processableCells = cells.Except(lockedCells).ToList();
+
+    for (int i = 0; i < noOfIterations; i++)
+    {
+        processableCells.ForEach(w => w.ReevaluateState());
+        processableCells.ForEach(w => w.UpdateState());
+
+        if (printAtEveryStep)
+            Console.WriteLine($"Generation {i + 1}: {cells.Count(w => w.IsOn)} lights are on.");
+    }
 }
 
 class Cell : IWorldObject
