f46a914a953e56f0479f1b632e929b2b8fd1de7e
David Božjak <bozjak@gmail.com>
2022-09-17 19:02:15 +0200

    Solution day 20 both parts

---
+++ Day20_InfiniteElves/Program.cs
@@ -0,0 +1,34 @@
+﻿var thresholdNumber = 34000000;
+
+var maxGifts10AtHouse = int.MinValue;
+var maxGifts11AtHouse = int.MinValue;
+
+for (int house = 780000; maxGifts10AtHouse < thresholdNumber || maxGifts11AtHouse < thresholdNumber; house++)
+{
+    int gifts10AtHouse = 10;
+    int gifts11AtHouse = 11;
+
+    for (int elf = 2; elf <= house; elf++)
+    {
+        if (house % elf != 0) continue;
+
+        gifts10AtHouse += elf * 10;
+
+        if (house > elf * 50)
+            continue;
+
+        gifts11AtHouse += elf * 11;
+    }
+
+    if (gifts10AtHouse > maxGifts10AtHouse)
+    {
+        maxGifts10AtHouse = gifts10AtHouse;
+        Console.WriteLine($"Part 1: House {house} gets {gifts10AtHouse} gifts");
+    }
+
+    if (gifts11AtHouse > maxGifts11AtHouse)
+    {
+        maxGifts11AtHouse = gifts11AtHouse;
+        Console.WriteLine($"Part 2: House {house} gets {gifts11AtHouse} gifts");
+    }
+}
