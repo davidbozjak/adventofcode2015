944a1fb79f75ad1a3a3d1b5d345d5c66590571e9
David Božjak <bozjak@gmail.com>
2022-09-18 10:53:47 +0200

    Added day 25

---
+++ Day25_Code/Program.cs
@@ -0,0 +1,48 @@
+﻿long firstCode = 20151125;
+
+var soughtIteration = GetIndexOfCoordinate(3019, 3010);
+
+var code = GenerateCodeN(firstCode, soughtIteration);
+
+Console.WriteLine(code);
+
+static int GetIndexOfCoordinate(int soughtX, int soughtY)
+{
+    int noOfGenerated = 0;
+
+    for (int diagnoal = 1; ; diagnoal++)
+    {
+        foreach (var coordinate in GetDiagnoalCoordinates(diagnoal))
+        {
+            if (coordinate.x == soughtX && coordinate.y == soughtY)
+                return noOfGenerated;
+
+            noOfGenerated++;
+        }
+    }
+
+    static IEnumerable<(int x, int y)> GetDiagnoalCoordinates(int diagnoal)
+    {
+        for (int x = 1, y = diagnoal; y >= 1; x++, y--)
+        {
+            yield return (x, y);
+        }
+    }
+}
+
+static long GenerateCodeN(long firstCode, long N)
+{
+    long code = firstCode;
+
+    for (int i = 0; i < N; i++)
+    {
+        code = GenerateCode(code);   
+    }
+
+    return code;
+}
+
+static long GenerateCode(long previousCode)
+{
+    return (previousCode * 252533) % 33554393;
+}
